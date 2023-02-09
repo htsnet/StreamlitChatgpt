d4e5411b77e1eb3bd2ebf13b8e7ecd2cce0a6b7b
Your NameHamilton <hamilton@hts-net.com.br>
2023-02-09 13:48:46 -0300

    News and improvements

--- README.md
+++ README.md
@@ -1,14 +1,16 @@
+Project: Project in Python using ChatGPT for analysis and text processing, using Streamlit for the visual part.
+Autor: Hamilton Tenório da Silva
+Date: feb/09/2023
 
 
 
------- Instruções ------
-
+------ Instructions ------
 pip install virtualenv
 python -m venv Ambiente_Virtual
 cd Ambiente_Virtual\Scripts
 activate
 cd ../..
-streamlit hello
-
-instalar pacotes que serão usados
+pip install <packages>
 streamlit run app.py
+
+One text to test: "Se o pessoal vê as minhas três vontades engordando desse jeito e crescendo que nem balão, eles vão rir, aposto. Eles não entendem essas coisas, acham que é infantil, não levam a sério. Eu tenho que achar depressa um lugar pra esconder as três: se tem coisa que eu não quero mais é ver gente grande rindo de mim."
--- app.py
+++ app.py
@@ -1,41 +1,91 @@
 import streamlit as st
 import openai
+from io import StringIO
 
 # Insira sua chave de API aqui
 openai.api_key = st.secrets['api_key_openai']
 
 def revise_text(text, acao):
-    completions = openai.Completion.create(
-        engine="text-davinci-002",
-        prompt=acao + '"' + text + ' "',
-        max_tokens=1024,
-        n=1,
-        stop=None,
-        temperature=0.5,
-    )
+    with st.spinner('Wait for it...'):
+        completions = openai.Completion.create(
+            engine="text-davinci-002",
+            prompt=acao + '"' + text + ' "' + ", keep the same language of this text",
+            max_tokens=1024,
+            n=1,
+            stop=None,
+            temperature=0.7,
+        )
 
     message = completions.choices[0].text
+    # print(message)
     return message
 
-#colunas
-col1, col2 = st.columns([0.9, 0.1])
+def check_text(text):
+    if text:
+        return True
+    st.info('The text field is empty!', icon="⚠️")
+    return False
+
 
 # título
-Titulo = f'Análise e melhoria de Textos com IA (ChatGPT)'
-col1.title(Titulo)
+Title = f'Analysis and Improvement of Texts with AI (ChatGPT)'
+st.title(Title)
 
-text = st.text_area("Cole o seu texto na caixa abaixo. Depois, clique no botão desejado.")
-# textxsss = "Se o pessoal vê as minhas três vontades engordando desse jeito e crescendo que nem balão, eles vão rir, aposto. Eles não entendem essas coisas, acham que é infantil, não levam a sério. Eu tenho que achar depressa um lugar pra esconder as três: se tem coisa que eu não quero mais é ver gente grande rindo de mim."
+#colunas
+col1, col2 = st.columns([0.7, 0.3])
 
-# escolha de ação a executar
-botResumo = st.button("Resumo do Texto")
-if botResumo:
-    revised_text = revise_text(text, "Faça um resumo rápido deste texto.")
-    # print(revised_text)
-    st.experimental_show(revised_text)
-    
-botSentimento = st.button("Sentimento do Texto")
-if botSentimento:
-    revised_text = revise_text(text, "Qual o sentimento deste texto?")
-    # print(revised_text)
-    st.experimental_show(revised_text)    
+with col1:
+    text_base = st.empty()
+
+    text = st.text_area("Paste your text into the box below.", max_chars=10000, height=400)
+    # textxsss = "Se o pessoal vê as minhas três vontades engordando desse jeito e crescendo que nem balão, eles vão rir, aposto. Eles não entendem essas coisas, acham que é infantil, não levam a sério. Eu tenho que achar depressa um lugar pra esconder as três: se tem coisa que eu não quero mais é ver gente grande rindo de mim."
+
+with col2:
+    uploaded_file = st.file_uploader("Or choose a file")
+    if uploaded_file is not None:
+        # To read file as bytes:
+        bytes_data = uploaded_file.getvalue()
+        st.write(bytes_data)
+        # text.text_area = bytes_data
+
+
+tab1, tab2, tab3, tab4 = st.tabs(['Summary', 'Sentiment', 'Rewriting', 'Change Style', ])
+
+
+# show tabs to choose the action
+with tab1:
+    st.write('Make a short summary of the text')
+    botSummary = st.button("Text Summary")
+    if botSummary:
+        if check_text(text):
+            revised_text = revise_text(text, "Faça um resumo rápido deste texto: ")
+            st.write(revised_text)
+
+with tab2:    
+    st.write('Identify the principal sentiment')
+    botSentiment = st.button("Text Sentiment")
+    if botSentiment:
+        if check_text(text):
+            revised_text = revise_text(text, "Qual o sentimento deste texto? ")
+            st.write(revised_text)    
+        
+with tab3:        
+    st.write('Rewrite and try to improve the text')
+    botRewriting = st.button("Text Rewriting")
+    if botRewriting:
+        if check_text(text):
+            revised_text = revise_text(text, "Reescreva e melhore o texto: ")
+            st.write(revised_text)   
+
+with tab4:        
+    st.write('Change the style to humorous')
+    botStyle = st.button("Change Style")
+    if botStyle:
+        if check_text(text):
+            revised_text = revise_text(text, "Reescreva o texto em estilo humorístico: ")
+            st.write(revised_text)                  
+
+st.header('Usage guidance')
+st.write('1 - Use a text copied from another location as a base and paste it in the indicated box. The limit is 5000 characters. You can also upload an file instead of paste the text.')
+st.write('2 - Choose the tab desired and press the button to perform the action.')
+st.write('3 - The final text will be in the same language as the original.')
