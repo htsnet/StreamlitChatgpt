c70e0d35eb1bd6d75a39001a78ac831501a1fa9e
Your NameHamilton <hamilton@hts-net.com.br>
2023-02-09 11:28:26 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+
+
+
+------ Instruções ------
+
+pip install virtualenv
+python -m venv Ambiente_Virtual
+cd Ambiente_Virtual\Scripts
+activate
+cd ../..
+streamlit hello
+
+instalar pacotes que serão usados
+streamlit run app.py
---
+++ app.py
@@ -0,0 +1,44 @@
+import streamlit as st
+import openai
+import os
+
+openai_api_key = os.getenv("OPENAI_API_KEY")
+
+# Insira sua chave de API aqui
+openai.api_key = "token"
+
+def revise_text(text, acao):
+    completions = openai.Completion.create(
+        engine="text-davinci-002",
+        prompt=acao + '"' + text + ' "',
+        max_tokens=1024,
+        n=1,
+        stop=None,
+        temperature=0.5,
+    )
+
+    message = completions.choices[0].text
+    return message
+
+#colunas
+col1, col2 = st.columns([0.9, 0.1])
+
+# título
+Titulo = f'Análise e melhoria de Textos com IA (ChatGPT)'
+col1.title(Titulo)
+
+text = st.text_area("Cole o seu texto na caixa abaixo. Depois, clique no botão desejado.")
+# textxsss = "Se o pessoal vê as minhas três vontades engordando desse jeito e crescendo que nem balão, eles vão rir, aposto. Eles não entendem essas coisas, acham que é infantil, não levam a sério. Eu tenho que achar depressa um lugar pra esconder as três: se tem coisa que eu não quero mais é ver gente grande rindo de mim."
+
+# escolha de ação a executar
+botResumo = st.button("Resumo do Texto")
+if botResumo:
+    revised_text = revise_text(text, "Faça um resumo rápido deste texto.")
+    # print(revised_text)
+    st.experimental_show(revised_text)
+    
+botSentimento = st.button("Sentimento do Texto")
+if botSentimento:
+    revised_text = revise_text(text, "Qual o sentimento deste texto?")
+    # print(revised_text)
+    st.experimental_show(revised_text)    
